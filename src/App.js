4726fab48e39026a34299c8045362569d6267405
komal <kk6612465@gmail.com>
2023-04-27 00:24:07 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex  flex-col ">
+        <div className="flex justify-center bg-indigo-500">
+          <h1>Expense Tracker</h1>
+        </div>
+        <div className=" flex flex-col justify-center bg-blue-500 ">
+          <div>Your Balance</div>
+          <div>$260</div>
+        </div>
+        <div className="flex flex-row  h-35 w-70">
+          <div className="border-2 bg-red-500">Income</div>
+          <div className="border-2 bg-red-500">Expenses</div>
+        </div>
+        <div>
+          <h1>History</h1>
+          <div className="flex flex-row justify-between w-1/2a">
+            <div>Cash</div>
+            <div>+500</div>
+          </div>
+          <div className="flex flex-row justify-between w-1/2a">
+            <div>Bank</div>
+            <div>-40</div>
+          </div>
+          <div className="flex flex-row justify-between w-1/2a">
+            <div>Camera</div>
+            <div>-200</div>
+          </div>
+        </div>
+        <h1>Add New Transcation</h1>
+        <div>
+          <form>
+            <label>Text</label>
+            <br></br>
+            <input type="text" placeholder="Enter Text"></input>
+            <br></br>
+            <label>Amount</label>
+            <br></br>
+            <input type="text" placeholder="Enter amount"></input>
+            <br></br>
+            <button
+              type="button"
+              className="bg-indigo-500 border border-sky-500  border-8"
+            >
+              Add Transcation
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
